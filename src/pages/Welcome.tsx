dd70c16499f09cd6e64a18f6a746c5c1eac6baf4
dezhizhang <1541609448@qq.com>
2022-05-15 14:29:10 +0800

    feat:update

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Button } from 'antd'
 import { PageContainer } from '@ant-design/pro-layout';
 import {  MicroAppWithMemoHistory } from 'umi';
 import styles from './Welcome.less';
 
 
 const Welcome: React.FC = () => {
-
+  const handleClick = () => {
+    return <MicroAppWithMemoHistory name='sales' url="/change"/>
+  }
   return (
     <PageContainer>
        <div className={styles.container}>
-         <a href='/info'>跳转</a>
-      <MicroAppWithMemoHistory name='sales' url="/info"/>
+         <Button type='primary' onClick={handleClick}>跨转</Button>
+         <a href='/sales/info'>跳转</a>
+      <MicroAppWithMemoHistory name='sales' autoSetLoading={true} url="/info"/>
     </div>
     </PageContainer>
   );
